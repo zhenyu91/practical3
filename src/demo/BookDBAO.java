d948a01920790434670fb102c33677763fe457d1
chitboon <chitboon@gmail.com>
2015-10-29 12:49:57 +0800

    Initial commit

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,28 @@
+package demo;
+/**
+ * Created by chitboon on 10/23/15.
+ */
+public class BookDBAO {
+    // get book details given the bookId
+    // this method is dummy for now
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    // convenient method to create a dummy book detail
+    // this method is meant for testing
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chitboon
+  Date: 10/22/15
+  Time: 1:45 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
