93080bacdfd05d9f900c1a5b474f1832a803fdcb
leecb <chitboon@gmail.com>
2016-10-31 12:20:56 +0800

    Revert to Skeleton code for Practical 2 for lab PC

--- src/demo/BookCatalogServlet.java
+++ src/demo/BookCatalogServlet.java
@@ -20,14 +20,38 @@ public class BookCatalogServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            BookDBAO db = new BookDBAO();
-            List<BookDetails> list = db.getAllBook();
-            request.setAttribute("books", list);
-            getServletContext().getRequestDispatcher("/bookcatalog.jsp").forward(request, response);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ServletException(e);
+        PrintWriter out = response.getWriter();
+
+        BookDBAO db = new BookDBAO();
+        List<BookDetails> list = db.getAllBook();
+        out.println("<html>" + "<head><title>Duke's Bookstore</title></head>" +
+                "<body  bgcolor=\"#ffffff\">" + "<center>" +
+                "<hr> <br> &nbsp;" + "<h1>" +
+                "<font size=\"+3\" color=\"#CC0066\">Duke's </font> <img src=\"" +
+                "./duke.books.gif\" alt=\"Duke holding books\"\">" +
+                "<font size=\"+3\" color=\"black\">Bookstore</font>" + "</h1>" +
+                "</center>" + "<br> &nbsp; <hr> <br> ");
+
+        out.println("<br> &nbsp;" + "<h3>Please Choose from our selection" +
+                "</h3>" + "<center> <table summary=\"layout\">");
+
+        for (BookDetails book : list) {
+            String bookId = book.getBookId();
+            out.println("<tr>" + "<td bgcolor=\"#ffffaa\">" + "<a href=\"" +
+                    response.encodeURL(request.getContextPath() +
+                            "/bookdetails?bookId=" + bookId) + "\"> <strong>" +
+                    book.getTitle() + "&nbsp; </strong></a></td>" +
+                    "<td bgcolor=\"#ffffaa\" rowspan=2>" + book.getPrice() +
+                    "&nbsp; </td>" + "<td bgcolor=\"#ffffaa\" rowspan=2>" +
+                    "<a href=\"" +
+                    response.encodeURL(request.getContextPath() +
+                            "/bookcatalog?bookId=" + bookId) + "\"> &nbsp;" +
+                    "Add to Cart&nbsp;</a></td></tr>" +
+                    "<tr>" + "<td bgcolor=\"#ffffff\">" + "&nbsp; &nbsp;" +
+                    "by<em> " + book.getFirstName() +
+                    " " + book.getSurname() + "</em></td></tr>");
         }
+
+        out.println("</table></center></body></html>");
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,10 +1,10 @@
 <html>
 <head><title>Duke's Bookstore</title></head>
 
-<%@ page import="model.*" %>
+<%@ page import="demo.*" %>
 <%
-    BookDAO db = new BookDAO();
-    BooksEntity book = db.getBook();
+    BookDBAO db = new BookDBAO();
+    BookDetails book = db.getBookDetails("1001");
 %>
 
 <body bgcolor="#ffffff">
@@ -16,7 +16,7 @@
     <hr>
 </center>
 <br> <b>What we are reading</b>
-<blockquote><em><a href="bookdetails?bookId=<%= book.getId() %>"><%= book.getTitle() %></a></em> What a cool book.
+<blockquote><em><a href="bookdetails?bookId=<%= book.getBookId() %>"><%= book.getTitle() %></a></em> What a cool book.
 </blockquote>
 <p><a href="bookcatalog"><b>Start Shopping</b></a></p>
 </body>
